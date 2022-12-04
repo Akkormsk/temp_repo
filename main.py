9b271aaecf437251803cb6e73a76a3407596443c
akkormsk <akkormsk@gmail.com>
2022-12-04 17:42:42 +0300

    done

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 def main():
-    pass
+    n = int(input('Введите количество элементов: '))
+    arr = []
+    for _ in range(n):
+        inp = input(f'Введите значение: ')
+        arr.append(inp)
+    arr_res = []
+    for st in arr:
+        if len(st) <= 3:
+            arr_res.append(st)
+    if arr_res:
+        print(arr_res)
+    else:
+        print('Нет элементов')
 
 
 if __name__ == '__main__':
